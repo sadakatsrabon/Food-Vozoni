17e67bfe8d9bb40c0a336872a32092df79acfb94
Sadakat Hossain <md.hossain.sadakat@gmail.com>
2023-06-22 17:02:59 +0600

    added a testimonials

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -7,6 +7,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { useEffect, useState } from "react";
 // import { Rating } from "@smastrom/react-rating";
+import Rating from "react-rating";
 // import '@smastrom/react-rating/style.css'
 
 
@@ -14,7 +15,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/reviews')
+        fetch('reviews.json')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
@@ -27,7 +28,6 @@ const Testimonials = () => {
             ></SectionTitle>
 
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-
                 {
                     reviews.map(review => <SwiperSlide
                         key={review._id}
