40f6ca95db37a4328df76f765236fc792cc08661
Sadakat Hossain <md.hossain.sadakat@gmail.com>
2023-08-10 08:40:54 +0600

    Use React Hook Form In SignUp Page

--- src/pages/SignUp/SingUp.jsx
+++ src/pages/SignUp/SingUp.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import { useForm } from "react-hook-form"
 
 const SingUp = () => {
+
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,27 +17,33 @@ const SingUp = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                         {/* Name */}
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                            <input type="text" {...register("name", { required: true })} name='name' placeholder="Your Name" className="input input-bordered" />
+                            {errors.name && <span><small className="text-red-600">Name is required</small></span>}
                         </div>
                         {/* Email */}
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text"></span>
                             </label>
-                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            <input type="email" {...register("email", { required: true })} name='email' placeholder="email" className="input input-bordered" />
+                            {errors.email && <span><small className="text-red-600">Email is required</small></span>}
                         </div>
                         {/* Password */}
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <input type="password" {...register("password", { required: true, minLength: 6, maxLength: 20 })} name='password' placeholder="password" className="input input-bordered" />
+                            {errors.password?.type === "required" && (
+                                <p className="text-red-600"><small>Password is required</small></p>
+                            )}
+                            {/* {errors.password && <span><small className="text-red-600">Password is required</small></span>} */}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -37,7 +51,7 @@ const SingUp = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Login</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
