0eccd815543480c53e18fd18469fcb426a59ccf9
Sadakat Hossain <sadakat.hossain11@gmail.com>
2023-08-18 23:17:11 +0600

    Login/LogOut button Fixed

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -2,14 +2,22 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const NavBar = () => {
-    const { user,logOut } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
 
-    const hadnleLogOut = () =>{
+    const hadnleLogOut = () => {
         logOut()
-        .then(() =>{})
-        .catch(error => console.error(error))
+            .then(() => { })
+            .catch(error => console.error(error));
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'You Have Successfull Logged Out',
+                showConfirmButton: false,
+                timer: 1500
+              })
     }
 
 
@@ -17,7 +25,6 @@ const NavBar = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
         <li><Link to="/order/salad">Order Food</Link></li>
-        <li><Link to="/login">Login</Link></li>
         {
             user ? <>
                 <span>{user?.displayName}</span>
